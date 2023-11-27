befea0856f1b953493eb8b56e1396dea0575c9df
AnnaSyrtseva <77345297+AnnaSyrtseva@users.noreply.github.com>
2023-11-27 23:50:09 +0100

    Add files via upload
    added PizzaTempoPage

---
+++ src/main/java/by/itacademy/asyrtseva/PizzaTempoPage.java
@@ -0,0 +1,39 @@
+package by.itacademy.asyrtseva;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class PizzaTempoPage {
+
+        private String pizzaMenuButton = "/html/body/div[1]/div[3]/ul/li[1]";
+        private String pizzaSize26Button = "//*[@id=\"1\"]/div/div[2]/div[2]/div[2]";
+        private String margaritaOrderButton = "//*[@id=\"1\"]/div/div[2]/div[3]/div[3]";
+        private String submitOrderButton = "//*[@id=\"order_pizza_popup\"]/div[2]/div[2]/div[2]/div[1]/div[3]/div[4]";
+        private String busketHomeButton = "//*[@id=\"basket_placeholder\"]/div";
+        private String pizzaInBusket = "//*div [@name=\"cart-product-name title\"]";
+
+        private WebDriver driver;
+        public PizzaTempoPage (WebDriver driver) {
+            this.driver = driver;
+        }
+
+        public void clickPizzaMenuButton() {driver.findElement(By.xpath(pizzaMenuButton)).click();}
+
+        public void clickPizzaSize26Button() {driver.findElement(By.xpath(pizzaSize26Button)).click();}
+
+        public void clickMargaritaOrderButton() {driver.findElement(By.xpath(margaritaOrderButton)).click();}
+
+        public void clickSubmitOrderButton() {driver.findElement(By.xpath(submitOrderButton)).click();}
+
+        public void clickBusketHomeButton() {driver.findElement(By.xpath(busketHomeButton)).click();}
+
+        public String getTextPizzaInBusket(){return driver.findElement(By.xpath(pizzaInBusket)).getText();
+
+                WebDriverWait wait = new WebDriverWait(driver, "30");
+                wait.until(ExpectedConditions.visibilityOfElementLocated(By.name(pizzaInBusket)));
+
+        }
+
+    }
